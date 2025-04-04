eb4ed4ee39af5899bdbab69beb51e890681642f8
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-04-04 18:21:46 -0300

    creo que se pudo la PWA

--- app/sw-register.js
+++ app/sw-register.js
@@ -1,14 +1,14 @@
 // Register the service worker
-if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js").then(
-            (registration) => {
-                console.log("Service Worker registration successful with scope: ", registration.scope)
-            },
-            (err) => {
-                console.log("Service Worker registration failed: ", err)
-            },
-        )
-    })
-}
+// if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+//     window.addEventListener("load", () => {
+//         navigator.serviceWorker.register("/sw.js").then(
+//             (registration) => {
+//                 console.log("Service Worker registration successful with scope: ", registration.scope)
+//             },
+//             (err) => {
+//                 console.log("Service Worker registration failed: ", err)
+//             },
+//         )
+//     })
+// }
 
